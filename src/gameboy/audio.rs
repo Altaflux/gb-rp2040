ee493f8230c8eb373338b765fa3e2e8b29747f00
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2024-09-29 19:13:06 -0700

    rust-gb now returns u16 audio

--- src/gameboy/audio.rs
+++ src/gameboy/audio.rs
@@ -1,16 +1,16 @@
-pub struct NullAudioPlayer;
+// pub struct NullAudioPlayer;
 
-impl gb_core::hardware::sound::AudioPlayer for NullAudioPlayer {
-    fn play(&mut self, _output_buffer: &[f32]) {
-        // Do nothing
-    }
+// impl gb_core::hardware::sound::AudioPlayer for NullAudioPlayer {
+//     fn play(&mut self, _output_buffer: &[f32]) {
+//         // Do nothing
+//     }
 
-    fn samples_rate(&self) -> u32 {
-        // 4096 / 1
-        5512
-    }
+//     fn samples_rate(&self) -> u32 {
+//         // 4096 / 1
+//         5512
+//     }
 
-    fn underflowed(&self) -> bool {
-        false
-    }
-}
+//     fn underflowed(&self) -> bool {
+//         false
+//     }
+// }
--- src/i2s2.rs
+++ src/i2s2.rs
@@ -106,23 +106,11 @@ where
     }
 
     fn process_audio(
-        output_buffer: &[f32],
+        output_buffer: &[u16],
         static_buffer: LimitingArrayReadTarget,
     ) -> LimitingArrayReadTarget {
         let output = static_buffer.new_max_read((output_buffer.len() * 1) as u32);
-
-        for (i, v) in output_buffer.chunks(2).enumerate() {
-            let frame: fon::Frame<fon::chan::Ch16, 2> = fon::Frame::<_, 2>::new(
-                fon::chan::Ch16::from(Ch32::new(v[0])),
-                fon::chan::Ch16::from(Ch32::new(v[1])),
-            );
-            let channels = frame.channels();
-            let ch1: u16 = <Ch16 as Into<i16>>::into(channels[0]) as u16;
-            let ch2: u16 = <Ch16 as Into<i16>>::into(channels[1]) as u16;
-
-            output.array[(i * 2) + 0] = ch1;
-            output.array[(i * 2) + 1] = ch2;
-        }
+        output.array[..output_buffer.len()].clone_from_slice(output_buffer);
         output
     }
 }
@@ -134,7 +122,7 @@ where
     P: PIOExt,
     SM: StateMachineIndex,
 {
-    fn play(&mut self, output_buffer: &[f32]) {
+    fn play(&mut self, output_buffer: &[u16]) {
         let dma_state = core::mem::replace(&mut self.dma_state, None).unwrap();
 
         match dma_state {
@@ -166,7 +154,7 @@ where
             None => false,
         };
 
-        underflowed
+        true
     }
 }
 
